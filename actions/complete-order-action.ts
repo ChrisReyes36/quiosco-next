12d2364180176155c699269432c029add1840e8b
Christopher Reyes <christopherreyes456@gmail.com>
2026-07-12 22:19:15 -0600

    feat: agregando procesar orden

--- actions/complete-order-action.ts
+++ actions/complete-order-action.ts
@@ -1,5 +1,27 @@
 "use server";
 
-export async function completeOrder() {
-  console.log("Desde complete order");
+import { prisma } from "@/src/lib/prisma";
+import { OrderIdSchema } from "@/src/schema";
+import { revalidatePath } from "next/cache";
+
+export async function completeOrder(formData: FormData) {
+  const data = { orderId: formData.get("order_id") };
+  const result = OrderIdSchema.safeParse(data);
+
+  if (result.success) {
+    try {
+      await prisma.order.update({
+        where: {
+          id: result.data.orderId,
+        },
+        data: {
+          status: true,
+          orderReadyAt: new Date(Date.now()),
+        },
+      });
+      revalidatePath("/admin/orders");
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
